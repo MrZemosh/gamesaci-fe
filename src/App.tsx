41d44af4b17cd206365e74f48e2f1ee7eac3639e
PoTTyeS <herniserver123@gmail.com>
2022-11-18 20:07:21 +0100

    v5.0 Mobile version better fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,31 @@ import { tiscaliModule } from './modules/tiscali.module';
 import { konzolisteModule } from './modules/konzoliste.module';
 import { igamersModule } from './modules/igamers.module';
 import { vortexModule } from './modules/vortex.module';
+import useDeviceDetect from './hooks/useDeviceDetect';
 
 const App: FC = (): JSX.Element => {
   const [scrapedData, setScrapedData] = useState<any[]>([]);
   const [scrapedDataFiltered, setScrapedDataFiltered] = useState<any[]>([]);
+  const [isVisible, setIsVisible] = useState(true);
+  const [height, setHeight] = useState(0);
+  const { isMobile } = useDeviceDetect();
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenToScroll);
+    return () => window.removeEventListener('scroll', listenToScroll);
+  }, []);
+
+  const listenToScroll = () => {
+    let heightToHideFrom = 200;
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    setHeight(winScroll);
+
+    if (winScroll > heightToHideFrom) {
+      isVisible && setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  };
 
   useEffect(() => {
     const T = async () => {
@@ -32,9 +53,15 @@ const App: FC = (): JSX.Element => {
       <div className="br-fade"></div>
       <div className="fixed flex justify-center items-center w-full pt-6 semi-md:px-28 lg:px-[21rem]  pb-6 shadow-xl backdrop-blur-2xl z-50">
         <div className="flex flex-col semi-md:flex-row w-full items-center justify-center gap-4 semi-md:gap-10">
-          <h1 className="text-4xl w-full text-center semi-md:text-start semi-md:w-1/2 my-3 md:my-0 font-bold uppercase text-white">
-            Gamesáci
-          </h1>
+          {isMobile && isVisible ? (
+            <h1 className="text-4xl w-full text-center semi-md:text-start semi-md:w-1/2 my-3 md:my-0 font-bold uppercase text-white">
+              Gamesáci
+            </h1>
+          ) : (
+            <h1 className="text-4xl w-full text-center semi-md:text-start semi-md:w-1/2 my-3 md:my-0 font-bold uppercase text-white">
+              Gamesáci
+            </h1>
+          )}
           <Filter
             onChange={(items: any[]) => {
               if (items.length === 0) return setScrapedDataFiltered(scrapedData);
